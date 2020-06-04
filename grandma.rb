68ce2767dc4622d17c1c74ebec3e371680835baf
Dean Erickson <dmepilot@yahoo.com>
2020-06-04 18:10:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  elsif phrase == (phrase.upcase && !"I LOVE YOU GRANDMA!"
     puts "NO, NOT SINCE 1938!"
 end
